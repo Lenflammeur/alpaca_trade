2f3ff3933c0350e0c4c5975dfe7cbc66a5b0ac88
Lenflammeur <t.soufiane@hotmail.fr>
2023-08-18 19:27:10 +0200

    Pair Trade

--- trading.py
+++ trading.py
@@ -9,7 +9,7 @@ response = sns.publish(
     TopicArn='arn:aws:sns:us-east-1:429690615505:MyOrders', Message="Connected to SNS"
 )
 
-lookback = 120
+lookback = 20
 threshold = 1.5
 
 def update_dataframe(df, bar):
@@ -28,83 +28,55 @@ def pair_trading(df, api):
     pair trading strategy with Apple and Microsoft
     """
     if len(df) < lookback:
-        if lookback % 10 == 0:
-             response = sns.publish(
-                TopicArn='arn:aws:sns:us-east-1:429690615505:MyOrders', Message=f"We have {lookback} lookback"
-            )
         return
     
-    if len(df) == lookback:
-            #print("We have enough data for long moving average")
-            response = sns.publish(
-                TopicArn='arn:aws:sns:us-east-1:429690615505:MyOrders', Message="We have enough data for pair trading strategy"
-            )
+    aapl_prices = df[df['symbol'] == 'AAPL']['price'].astype(float)
+    msft_prices = df[df['symbol'] == 'MSFT']['price'].astype(float)
 
-    aapl_prices = df[df['symbol'] == 'AAPL']['price']
-    msft_prices = df[df['symbol'] == 'MSFT']['price']
+    if len(aapl_prices) < lookback or len(msft_prices) < lookback:
+        print("Not enough data for trading")
+        return
+    
+    if len(aapl_prices) == lookback and len(msft_prices) == lookback:
+        sns.publish(TopicArn='arn:aws:sns:us-east-1:429690615505:MyOrders', Message="It's Show Time!")
 
-    spread = aapl_prices - msft_prices
-    mean_spread = spread.rolling(window=lookback).mean()
-    std_spread = spread.rolling(window=lookback).std()
-    z_score = (spread - mean_spread) / std_spread
+    aapl_prices = aapl_prices.tail(lookback)
+    msft_prices = msft_prices.tail(lookback)
 
-    portfolio_value = api.get_account().cash
+    spread = aapl_prices.values - msft_prices.values
+    spread_series = pd.Series(spread)
+    mean_spread = spread_series.rolling(window=lookback).mean().iloc[-1]
+    std_spread = spread_series.rolling(window=lookback).std().iloc[-1]
+    z_score = (spread[-1] - mean_spread) / std_spread
 
-    qty_aapl = int(0.5 * portfolio_value / aapl_prices.iloc[-1])
-    qty_msft = int(0.5 * portfolio_value / msft_prices.iloc[-1])
+    portfolio_value = float(api.get_account().cash)
+    qty_aapl = int(0.4 * portfolio_value / aapl_prices.iloc[-1])
+    qty_msft = int(0.4 * portfolio_value / msft_prices.iloc[-1])
 
-    if z_score[-1] > threshold:
-        api.submit_order(
-            symbol='AAPL',
-            qty=qty_aapl,
-            side='sell',
-            type='market',
-            time_in_force='gtc',
-        )
-        api.submit_order(
-            symbol='MSFT',
-            qty=qty_msft,
-            side='buy',
-            type='market',
-            time_in_force='gtc',
-        )
-        # After submitting the order, publish a message to SNS
-        response = sns.publish(
-            TopicArn='arn:aws:sns:us-east-1:429690615505:MyOrders', Message=f"Submitted {qty_aapl} sell for AAPL & {qty_msft} buy for MSFT"
-        )
-    elif z_score[-1] < -threshold:
-        api.submit_order(
-            symbol='AAPL',
-            qty=qty_aapl,
-            side='buy',
-            type='market',
-            time_in_force='gtc',
-        )
-        api.submit_order(
-            symbol='MSFT',
-            qty=qty_msft,
-            side='sell',
-            type='market',
-            time_in_force='gtc',
-        )
-        response = sns.publish(
-            TopicArn='arn:aws:sns:us-east-1:429690615505:MyOrders', Message=f"Submitted {qty_aapl} buy for AAPL & {qty_msft} sell for MSFT"
-        )
-    elif abs(z_score[-1]) < 0.5:
-        api.submit_order(
-            symbol='AAPL',
-            qty=qty_aapl,
-            side='sell',
-            type='market',
-            time_in_force='gtc',
-        )
-        api.submit_order(
-            symbol='MSFT',
-            qty=qty_msft,
-            side='sell',
-            type='market',
-            time_in_force='gtc',
-        )
-        response = sns.publish(
-            TopicArn='arn:aws:sns:us-east-1:429690615505:MyOrders', Message=f"Submitted {qty_aapl} sell for AAPL & {qty_msft} sell for MSFT"
-        )
+    positions = api.list_positions()
+    long_aapl = any(pos.symbol == 'AAPL' and pos.side == 'long' for pos in positions)
+    short_aapl = any(pos.symbol == 'AAPL' and pos.side == 'short' for pos in positions)
+    long_msft = any(pos.symbol == 'MSFT' and pos.side == 'long' for pos in positions)
+    short_msft = any(pos.symbol == 'MSFT' and pos.side == 'short' for pos in positions)
+
+    if z_score > threshold:
+        if not long_aapl and not short_msft:
+            api.submit_order(symbol='AAPL', qty=qty_aapl, side='buy', type='market', time_in_force='gtc')
+            api.submit_order(symbol='MSFT', qty=qty_msft, side='sell', type='market', time_in_force='gtc')
+            sns.publish(TopicArn='arn:aws:sns:us-east-1:429690615505:MyOrders', Message=f"Opened positions: Bought {qty_aapl} AAPL & sold {qty_msft} MSFT")
+
+    elif z_score < -threshold:
+        if not short_aapl and not long_msft:
+            api.submit_order(symbol='AAPL', qty=qty_aapl, side='sell', type='market', time_in_force='gtc')
+            api.submit_order(symbol='MSFT', qty=qty_msft, side='buy', type='market', time_in_force='gtc')
+            sns.publish(TopicArn='arn:aws:sns:us-east-1:429690615505:MyOrders', Message=f"Opened positions: Sold {qty_aapl} AAPL & bought {qty_msft} MSFT")
+
+    elif abs(z_score) < 0.5:
+        if long_aapl and short_msft:
+            api.submit_order(symbol='AAPL', qty=qty_aapl, side='sell', type='market', time_in_force='gtc')
+            api.submit_order(symbol='MSFT', qty=qty_msft, side='buy', type='market', time_in_force='gtc')
+            sns.publish(TopicArn='arn:aws:sns:us-east-1:429690615505:MyOrders', Message=f"Closed positions: Sold {qty_aapl} AAPL & bought {qty_msft} MSFT")
+        elif short_aapl and long_msft:
+            api.submit_order(symbol='AAPL', qty=qty_aapl, side='buy', type='market', time_in_force='gtc')
+            api.submit_order(symbol='MSFT', qty=qty_msft, side='sell', type='market', time_in_force='gtc')
+            sns.publish(TopicArn='arn:aws:sns:us-east-1:429690615505:MyOrders', Message=f"Closed positions: Bought {qty_aapl} AAPL & sold {qty_msft} MSFT")
